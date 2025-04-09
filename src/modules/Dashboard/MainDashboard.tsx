65bc854c2297161e8b4270bae18f5f5555639038
atefeh <ativalimohammadi11@gmail.com>
2025-04-09 11:15:59 +0330

    fix main dashboard responsive

--- src/modules/Dashboard/MainDashboard.tsx
+++ src/modules/Dashboard/MainDashboard.tsx
@@ -34,7 +34,7 @@ const MainDashboard = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-24 gap-5 mt-6">
-        <div className="col-span-16 2xl:col-span-18 flex flex-col 2xl:flex-row gap-5 w-full">
+        <div className="col-span-1 md:col-span-16 2xl:col-span-18 flex flex-col 2xl:flex-row gap-5 w-full">
           <div className="w-full 2xl:w-1/2">
             <ProductOverview />
           </div>
@@ -42,9 +42,10 @@ const MainDashboard = () => {
             <RecentActivities />
           </div>
         </div>
-        <div className="col-span-8 2xl:col-span-6 w-full">
+
+        <div className="col-span-1 md:col-span-8 2xl:col-span-6 w-full">
           <ProductCard className="w-full" />
-          <ProxyUsage className="mt-5 w-full" />
+          <ProxyUsage className="w-full mt-5" />
         </div>
       </div>
     </div>
