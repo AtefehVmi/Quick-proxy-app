5d43ff769abe2d55ac2ca8d65c35cda673a159d0
atefeh <ativalimohammadi11@gmail.com>
2025-04-13 16:17:56 +0330

    build date for headings

--- src/modules/shared/Heading.tsx
+++ src/modules/shared/Heading.tsx
@@ -1,8 +1,21 @@
 import React, { FC, SVGProps } from "react";
+import CalendarIcon from "public/icons/calendar.svg";
+import TextSm from "@/components/Typography/TextSm";
 
 type HeadingProps = { Icon: FC<SVGProps<SVGElement>>; title: string };
 
 const Heading: React.FC<HeadingProps> = ({ Icon, title }) => {
+  const getFormattedDate = () => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+
+    const today = new Date();
+    return today.toLocaleDateString("en-GB", options).toUpperCase();
+  };
+
   return (
     <div className="flex items-center justify-between w-full">
       <div className="flex items-center gap-3">
@@ -15,7 +28,12 @@ const Heading: React.FC<HeadingProps> = ({ Icon, title }) => {
           <p className="text-base text-grey-600">Welcome Back to Fastproxy</p>
         </div>
       </div>
-      <div></div>
+      <div className="bg-black-2 py-4 px-8 flex items-center gap-1">
+        <CalendarIcon />
+        <TextSm className="text-white font-semibold">
+          {getFormattedDate()}
+        </TextSm>
+      </div>
     </div>
   );
 };
